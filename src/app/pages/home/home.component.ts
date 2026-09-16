429ccd858e4cd75ffdc42c404df62eb9731db73b
heygoe41-sys <heygoe41@gmail.com>
2026-09-16 10:33:19 +0000

    ipdate card shop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  shopList= [
+    {
+      name:"KWA ACE eKITCHEN",
+      logo:'https://i.postimg.cc/qBmBLr6Q/1789483061963.jpg',
+      cover:'https://i.postimg.cc/rwBZqzY2/1789483155738.jpg'
+    },
+    {
+      name:'Goldern Dash',
+      cover:'https://i.postimg.cc/KjSBxcVj/cm6i5aq1k000003lebrgq2ki9.jpg'
+    }
+  ]
 }
